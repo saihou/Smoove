1bd55629e5d4993d82960c4319902f296a2bb34b
saihou <sai-hou@hotmail.com>
2015-10-24 15:02:20 -0700

    Added logic for Search

--- Android/app/src/main/java/hackathon/money2020/browncow/SearchActivity.java
+++ Android/app/src/main/java/hackathon/money2020/browncow/SearchActivity.java
@@ -1,20 +1,59 @@
 package hackathon.money2020.browncow;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Calendar;
 
 public class SearchActivity extends AppCompatActivity {
     ListView listView;
+    ImageButton btnSearch;
+    EditText txtSearch;
+    ArrayList<String> listOfRestaurants;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        listView = (ListView) findViewById(R.id.listView);
         setContentView(R.layout.activity_search);
+
+        listView = (ListView) findViewById(R.id.listView);
+        listOfRestaurants = new ArrayList<String>();
+        adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_spinner_item, listOfRestaurants);
+        listView.setAdapter(adapter);
+
+        txtSearch = (EditText) findViewById(R.id.searchBar);
+        txtSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    performSearch();
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        btnSearch = (ImageButton) findViewById(R.id.searchButton);
+        btnSearch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                performSearch();
+            }
+        });
     }
 
     @Override
@@ -40,7 +79,31 @@ public class SearchActivity extends AppCompatActivity {
         if (id == android.R.id.home) {
             this.finish();
         }
-
         return super.onOptionsItemSelected(item);
+    }
+
+    public void performSearch() {
+        ///******///******////*****////*****/////****///*////*///
+        ///*////////*////////*/////*///*////*///*///////*////*///
+        ///******///******///*******///******///*///////******///
+        ////////*///*////////*/////*///*/*//////*///////*////*///
+        ///******///******///*/////*///*//***////****///*////*///
+        String searchPhrase = txtSearch.getText().toString().trim();
+
+        if (searchPhrase.length() > 2) {
+            //search
+            addToListView("omg");
+
+            Toast.makeText(getBaseContext(), "Searching for " + searchPhrase + "!", Toast.LENGTH_SHORT).show();
+        } else if (searchPhrase.length() > 0){
+            Toast.makeText(getBaseContext(), "Type something longer damn it", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getBaseContext(), "You gotta type something", Toast.LENGTH_SHORT).show();
+            txtSearch.clearFocus();
+        }
+    }
+
+    public void addToListView(String item) {
+        adapter.add(item);
     }
 }
