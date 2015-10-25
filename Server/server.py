a526d83b58be79a2f139e790b32c4ff2c538ce6d
Jack Ong <jackong2013@gmail.com>
2015-10-25 02:52:23 -0700

    Merge HTML with server

--- Server/server.py
+++ Server/server.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, jsonify
 import mysql.connector as mysql
 import json
 
@@ -7,10 +7,12 @@ app = Flask('Smoove')
 
 smoovedb = mysql.connect(user='browncowmaster', password='hownowbrowncow', host='browncow.ctjebouppjjs.us-west-2.rds.amazonaws.com', database='smoove')
 
+###### Web APP #####
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
 	cursor = smoovedb.cursor()
-	query = ("select * from test_table")
+	query = ("SELECT *  FROM reservations rs inner join users_new u on u.idusers = rs.rsvn_user_id")
 	cursor.execute(query)
 	data = []
 	for item in cursor:
@@ -19,10 +21,19 @@ def index():
 		return None
 	return render_template('index.html', data=data)
 
-@app.route('/left')
-def left():
-	return render_template('left-sidebar.html')
+@app.route('/get_reservation_ajax')
+def get_reservation_ajax():
+	cursor = smoovedb.cursor()
+	query = ("SELECT rs.rsvn_date, rs.rsvn_user_id, rs.rsvn_merchant_id, rs.rsvn_pax, day(rs.rsvn_time_in) rsvn_day, hour(rs.rsvn_time_in) rsvn_hour, u.users_first_name FROM reservations rs inner join users_new u on u.idusers = rs.rsvn_user_id")
+	cursor.execute(query)
+	data = []
+	for item in cursor:
+		data.append(item)
+	return jsonify(result=data)
 
+###### Mobile App REST API #####
+
+# Get Transaction Records
 @app.route('/get_transaction/<user_id>')
 def get_txn(user_id):
 	cursor = smoovedb.cursor()
@@ -41,6 +52,8 @@ def get_txn(user_id):
 	data = json.dumps(data)
 	return data
 
+
+# Get Reservations Records
 @app.route('/get_reservation/<user_id>')
 def get_rsvn(user_id):
 	cursor = smoovedb.cursor()
@@ -59,12 +72,13 @@ def get_rsvn(user_id):
 	data = json.dumps(data)
 	return data
 
+# Get List of Available Restaurants
 @app.route('/get_restaurants')
 def get_restaurant():
 	cursor = smoovedb.cursor()
-	query = ("SELECT users_first_name, merchant_desc FROM users_new where type = 'merchant'")
+	query = ("SELECT idusers, users_first_name, merchant_desc FROM users_new where type = 'merchant'")
 	cursor.execute(query)
-	header = ['company_name', 'desc']
+	header = ['merchant_id', 'company_name', 'desc']
 	data = []
 	for item in cursor:
 		single_res = {}
@@ -73,6 +87,18 @@ def get_restaurant():
 		data.append(single_res)
 	data = json.dumps(data)
 	return data
+
+# POST a reservation
+@app.route('/reserve/<rsvn_data>', methods=['POST'])
+def reserve(rsvn_data):
+	data = json.loads(rsvn_data)
+	cursor = smoovedb.cursor()
+	query = ("insert into reservations (rsvn_date, rsvn_user_id, rsvn_merchant_id, \
+			rsvn_pax, rsvn_time_in values (%(date)s, %(user)s, %(merchant)s, %(pax)s), %(time)s", data)
+	cursor.execute(query)
+	return "reservation made"
+
+
 
 if __name__ == '__main__':
 	app.run(host="0.0.0.0", debug=True)
